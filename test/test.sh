43b03083b882e610742be6947b29cfaa11a90e91
Xavier Mirabelli-Montan <xavier@travelnation.co.uk>
2019-03-07 14:34:22 +0000

    Seeing if I can get docker to connect this time round

--- test/test.sh
+++ test/test.sh
@@ -5,4 +5,4 @@
 docker --version
 docker-compose --version
 
-# docker-compose -f ./infrastructure/docker-compose/docker-compose.yml -f ./infrastructure/docker-compose/docker-compose-dev.yml up -d
+docker-compose -f ./infrastructure/docker-compose/docker-compose.yml -f ./infrastructure/docker-compose/docker-compose-dev.yml up -d
