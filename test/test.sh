24d00bec388a7878bee0f337f9cb8d6c0ab3ccd2
Xavier Mirabelli-Montan <xavier@travelnation.co.uk>
2019-03-07 16:09:01 +0000

    Updating the test script

--- test/test.sh
+++ test/test.sh
@@ -2,4 +2,4 @@
 
 . ./scripts/test/setup.sh
 
-docker-compose -f ./infrastructure/docker-compose/docker-compose.yml -f ./infrastructure/docker-compose/docker-compose-dev.yml up -d
+docker-compose -f ./infrastructure/docker-compose/docker-compose.yml up -d
