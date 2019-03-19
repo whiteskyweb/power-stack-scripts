0d9edaba8a51f27e68d4bc1dab7126e0e60d3faf
Xavier Mirabelli-Montan <xavier@travelnation.co.uk>
2019-03-19 10:07:36 +0000

    Adding docker iamge

--- build/build.sh
+++ build/build.sh
@@ -3,13 +3,14 @@
 . ./scripts/build/setup.sh
 
 build_image () {
+
   printf "${GREEN_BG}Building ${2} image${NC}\n"
 
     docker build -t "${CI_REGISTRY}/${1}/${2}:dev"  -f "./infrastructure/docker/${1}-${2}/Dockerfile" .
-
     docker push "${CI_REGISTRY}/${1}/${2}:dev"
 
   printf "${GREEN}done${NC}\n"
+
 }
 
 
@@ -20,6 +21,8 @@ if hash docker 2>/dev/null; then
   build_image 'frontend' 'web'
 
 else
+
   echo >&2 "I require docker but it's not installed.  Aborting.";
   exit 1;
+
 fi
--- release/release.sh
+++ release/release.sh
@@ -3,12 +3,14 @@
 . ./scripts/release/setup.sh
 
 release_images () {
+
   printf "${GREEN_BG}Pushing image to registry${NC}\n"
 
     docker tag "${CI_REGISTRY}/${1}/${2}:dev" "${CI_REGISTRY}/${1}/${2}:${3}"
     docker tag "${CI_REGISTRY}/${1}/${2}:${3}" "${CI_REGISTRY}/${1}/${2}:latest"
 
   printf "${GREEN}done${NC}\n"
+
 }
 
 
@@ -19,7 +21,9 @@ if hash docker 2>/dev/null; then
   release_images 'frontend' 'web' $VERSION
 
 else
+
   echo >&2 "I require docker but it's not installed.  Aborting.";
   exit 1;
+
 fi
 
